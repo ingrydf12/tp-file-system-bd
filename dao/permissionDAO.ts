6b2cbed32c1bc4598ef4c382297102408a5c397e
Ingryd <ingrydcduarte@gmail.com>
2026-01-15 09:29:43 -0300

    draft: skeleton of permission service

--- dao/permissionDAO.ts
+++ dao/permissionDAO.ts
@@ -1,1 +1,21 @@
 // TODO
+import { pool } from "../database/connection";
+import { Permissao } from "../model/permissao";
+
+export class PermissionDAO {
+    async generatePermission(permissao: Permissao) {
+        
+    }
+
+    async update(){
+
+    }
+
+    async findPermissionByUser(){
+        
+    }
+
+    async remove(){
+
+    }
+}
--- service/permissaoService.ts
+++ service/permissaoService.ts
@@ -1,1 +1,15 @@
-// TODO
+import { PermissionDAO } from "../dao/permissionDAO";
+
+const permissionDAO = new PermissionDAO();
+
+export async function addPermission() {
+
+}
+
+export async function updatePermissions() {
+    
+}
+
+export async function removePermission() {
+
+}
